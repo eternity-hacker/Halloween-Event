f7cef5342feab45734a3af5ce8bf4e9f69f623f2
Olivia <eternity@hotlookbook.com>
2022-11-08 18:44:58 -0800

    Added player stats and ghosts array

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// Objective: User has three chances to find the four friendly ghosts. Each chance consists of two clicks. If the user finds a friendly ghost then the click counter is NOT increased, but if the user finds a scary ghost then the click counter is increased.
+
+const playerChances = 3;
+const playerClicks = 0;
+const doorsClicked = 0;
+const ghosts = [
+  "scary-ghost-1",
+  "scary-ghost-1",
+  "scary-ghost-1",
+  "scary-ghost-2",
+  "scary-ghost-2",
+  "scary-ghost-2",
+  "casper",
+  "casper",
+  "casper",
+  "casper",
+];
+
 const doorsContainer = document.querySelector("#doors-container");
 
 doorsContainer.addEventListener("click", function (event) {
@@ -7,6 +25,7 @@ doorsContainer.addEventListener("click", function (event) {
   // injects an the ghost()
   event.target.insertAdjacentHTML(
     "afterend",
-    `<img class="ghost" src="images/bad-ghost-1.png" alt="closed door">`
+    `<img class="ghost" src="images/${ghosts[doorsClicked]}.png" alt="closed door">`
   );
+  // Determine if the user found a friendly ghost or a scary ghost.
 });
