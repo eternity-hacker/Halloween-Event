2deabf5b85f84382f38346727b0f062f3180f7b9
Olivia <eternity@hotlookbook.com>
2022-11-09 19:56:42 -0800

    we implemented ghost randomization

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // Objective: User has three chances to find the four friendly ghosts. Each chance consists of two clicks. If the user finds a friendly ghost then the click counter is NOT increased, but if the user finds a scary ghost then the click counter is increased.
 
-const playerChances = 3;
-const playerClicks = 0;
-const doorsClicked = 0;
+let playerChances = 3;
+let playerClicks = 0;
+// Keeps track of how many doors the player has opened.
+let doorsClicked = 0;
 const ghosts = [
   "scary-ghost-1",
   "scary-ghost-1",
@@ -17,15 +18,16 @@ const ghosts = [
 ];
 
 const doorsContainer = document.querySelector("#doors-container");
-
 doorsContainer.addEventListener("click", function (event) {
+  let randomNumber = Math.floor(Math.random() * 10);
+  console.log(randomNumber);
   // opens the door
-  console.log(event);
   event.target.src = "images/opened-door.png";
   // injects an the ghost()
   event.target.insertAdjacentHTML(
     "afterend",
-    `<img class="ghost" src="images/${ghosts[doorsClicked]}.png" alt="closed door">`
+    `<img class="ghost" src="images/${ghosts[randomNumber]}.png" alt="closed door">`
   );
-  // Determine if the user found a friendly ghost or a scary ghost.
+  // increment the doors clicked variable
+  doorsClicked++;
 });
