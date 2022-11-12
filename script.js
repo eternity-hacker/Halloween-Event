fb19c59eae1e09e78e1c05d0778b4149efd774e9
Olivia <eternity@hotlookbook.com>
2022-11-11 16:31:01 -0800

    added querySelectors and updated player variables & ui

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 // Objective: User has three chances to find the four friendly ghosts. Each chance consists of two clicks. If the user finds a friendly ghost then the click counter is NOT increased, but if the user finds a scary ghost then the click counter is increased.
 
 let playerChances = 3;
-let playerClicks = 0;
+let playerStrikes = 0;
+let scaryGhostCounter = 0;
+let friendlyGhostCounter = 0;
+
 // Keeps track of how many doors the player has opened.
 let doorsClicked = 0;
 const ghosts = [
@@ -18,16 +21,47 @@ const ghosts = [
 ];
 
 const doorsContainer = document.querySelector("#doors-container");
+const fearMeter = document.querySelector("#fear-meter");
+const spanFriendly = document.querySelector(".friendly");
+const spanScary = document.querySelector(".scary");
+
 doorsContainer.addEventListener("click", function (event) {
   let randomNumber = Math.floor(Math.random() * 10);
-  console.log(randomNumber);
+
   // opens the door
   event.target.src = "images/opened-door.png";
+
   // injects an the ghost()
   event.target.insertAdjacentHTML(
     "afterend",
     `<img class="ghost" src="images/${ghosts[randomNumber]}.png" alt="closed door">`
   );
+
+  // check if random ghost is scary or friendly
+  if (
+    ghosts[randomNumber] === "scary-ghost-1" ||
+    ghosts[randomNumber] === "scary-ghost-2"
+  ) {
+    playerStrikes++;
+    scaryGhostCounter++;
+    spanScary.textContent = scaryGhostCounter;
+
+    // if the player strikes equals 2, then reset the strike counter.
+    if (playerStrikes === 2) {
+      playerStrikes = 0;
+      playerChances--;
+      fearMeter.value++;
+    }
+
+    console.log(`the current player strikes ${playerStrikes}`);
+    console.log(`you have ${playerChances} chances remaining`);
+  } else {
+    friendlyGhostCounter++;
+    spanFriendly.textContent = friendlyGhostCounter;
+  }
+
+  // check players strikes
+
   // increment the doors clicked variable
   doorsClicked++;
 });
